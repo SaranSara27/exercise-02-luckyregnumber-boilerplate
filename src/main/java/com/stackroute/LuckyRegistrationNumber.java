6ba8dfab22a741545de9785cbe048f298690b23d
ubuntu <ubuntu@ip-172-31-95-151.ec2.internal>
2020-03-18 09:05:14 +0000

    clean up

--- src/main/java/com/stackroute/LuckyRegistrationNumber.java
+++ src/main/java/com/stackroute/LuckyRegistrationNumber.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 //Complete the code below as given in the problem.md file
 public class LuckyRegistrationNumber {
 
-	public static int checkRegistrationNumber(String regNumber) {
-
-		String regex = "(KA|DL)(0[1-9]|10)[A-Z]{1,2}[1-9][0-9]{3}";
-		int output = -1;
-		if (regNumber.matches(regex))
-			output = 0;
-		if (output == 0) {
-			int num = Integer.valueOf(regNumber.substring(regNumber.length() - 4));
-			int sum = sumOfDigits(num);
-			if (sum > 9)
-				sum = sumOfDigits(sum);
-			if (sum == 6)
-				output = 1;
-		}
-
-		return output;
-	}
-
-	private static int sumOfDigits(int num) {
+   
+    public static int checkRegistrationNumber(String regNumber) {
+    	String regex = "(KA|DL)(0[1-9]|10)[A-Z]{1,2}[1-9][0-9]{3}";
+    	int output = -1;
+    	if (regNumber.matches(regex))
+    		output = 0;
+    	if (output == 0) {
+    		int num = Integer.valueOf(regNumber.substring(regNumber.length() - 4));
+    		int sum = sumOfDigits(num);
+    		if (sum > 9)
+    			sum = sumOfDigits(sum);
+    		if (sum == 6)
+    			output = 1;
+    	}
+    	return output;
+    }
+    
+    private static int sumOfDigits(int num) {
 		int sum = 0;
 		while (num != 0) {
 			sum = sum + num % 10;
@@ -32,19 +31,20 @@ public class LuckyRegistrationNumber {
 		return sum;
 	}
 
-	// Do not print anything other than what's expected/asked in problem
-	public static void main(String[] args) {
-		// Use Scanner to get input from console
-		Scanner scanner = new Scanner(System.in);
-		String regNumber = scanner.nextLine();
-		int output = checkRegistrationNumber(regNumber);
-		if (output == 1)
-			System.out.println("Lucky Registration Number");
-		else if (output == 0)
-			System.out.println("Valid Registration Number");
-		else if (output == -1)
-			System.out.println("Invalid Registration Number");
-
-	}
+   	//Do not print anything other than what's expected/asked in problem
+    public static void main(String[] args) {
+    	//Use Scanner to get input from console
+    	Scanner scanner = new Scanner(System.in);
+    	String regNumber = scanner.nextLine();
+    	int output = checkRegistrationNumber(regNumber);
+    	if (output == 1)
+    		System.out.println("Lucky Registration Number");
+    	else if (output == 0)
+    		System.out.println("Valid Registration Number");
+    	else if (output == -1)
+    		System.out.println("Invalid Registration Number");
+ 
+    
+    }
 
 }
